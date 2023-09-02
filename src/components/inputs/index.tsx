b044d887aa174e6c8f1d2f94cfa14cfc64ec6cac
themanuca <ssergioca@gmail.com>
2023-09-02 04:46:50 -0300

    adicionando validação aos inputs do login

--- src/components/inputs/index.tsx
+++ src/components/inputs/index.tsx
@@ -1,16 +1,29 @@
-import { IInputProps, Input as NativeBaseInput } from "native-base";
-export function Input({...res}:IInputProps){
+import { FormControl, IInputProps, Input as NativeBaseInput } from "native-base";
+
+type Props=IInputProps & {
+    errorMessage?:string | null;
+}
+export function Input({errorMessage = null,isInvalid,...res}:Props){
+    const invalid = !!errorMessage || isInvalid;
     return(
-        <NativeBaseInput
-            bg="#886058"
-            fontSize={'md'}
-            height={16}
-            marginBottom={4}
-            borderRadius={10}
-            _focus={{
-                backgroundColor:'#cdbea0'
-            }}
-            {...res}
-        />
+        <FormControl isInvalid={invalid} marginBottom={4}>
+            <NativeBaseInput
+                bg="#886058"
+                fontSize={'md'}
+                isInvalid={invalid}
+                height={16}
+                borderRadius={10}
+                _focus={{
+                    backgroundColor:'#cdbea0'
+                }}
+                {...res}
+                _invalid={{
+                    borderWidth:2
+                }}
+            />
+            <FormControl.ErrorMessage>
+               {errorMessage}
+            </FormControl.ErrorMessage>
+        </FormControl>
     );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,70 @@
-import { Center, Heading, VStack } from "native-base";
+import { Center, Heading, VStack, Text, Container } from "native-base";
 import { Input } from "../../components/inputs";
 import { Button } from "../../components/buttons";
+import { useForm, Controller } from "react-hook-form";
 
 export default function Login() {
-    return (
-      <VStack  flex={1} paddingX={10}  _light={{
-        bg: 'warmGray.100'
-      }} shadow={3}>
-        <Center>
-            <Heading margin={'3em'} marginTop={'5em'}color={'#fc640c'} >
-                Bem vindo !
-            </Heading>
-            <Input placeholder="E-mail"  placeholderTextColor={'primary.100'}/>
-            <Input placeholder="Senha" type="password" placeholderTextColor={'primary.100'}/>
-            <Button titulo="Acessar" bg={'#fc640cd1'}/>
-        </Center>
-      </VStack>
-    );
+
+  type LoginType = {
+    email:string;
+    senha:string;
   }
+  const {control, handleSubmit, formState:{errors}} = useForm<LoginType>();
+
+  function hundleLogin(data:LoginType){
+    console.log(data);
+  }
+
+  return (
+    <VStack  flex={1} paddingX={10}  _light={{
+      bg: 'warmGray.100'
+    }} shadow={3}>
+      <Center>
+          <Heading margin={'3em'} marginTop={'5em'}color={'#fc640c'} >
+              Bem vindo !
+          </Heading>
+          <Controller
+            control={control}
+            rules={{
+              required:'Informe seu E-mail',
+              pattern:{
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail Invalido'
+              }
+            }}
+            name="email"
+            render={({ field:{onChange} })=>(
+              <Input 
+              placeholder="E-mail" 
+              onChangeText={onChange}  
+              placeholderTextColor={'primary.100'}
+              errorMessage={errors.email?.message}
+              />
+
+            )}
+          />
+          <Controller
+          control={control}
+          rules={{
+            required:'Por favor, preencha todos os campos obrigatórios.'
+          }}
+          name="senha"
+          render={({ field:{onChange} })=>(
+            <Input 
+            placeholder="Senha" 
+            type="password"  
+            onChangeText={onChange}  
+            placeholderTextColor={'primary.100'}
+            errorMessage={errors.senha?.message}
+            />
+
+          )}
+          />
+          <Button titulo="Acessar" bg={'#fc640cd1'} onPress={handleSubmit(hundleLogin)}/>
+          <Text> Registre-se</Text>
+
+      </Center>
+    </VStack>
+  );
+}
   
